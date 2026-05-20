07e8303c0ad8f2913ee652f4f20103b912ade2a5
Tulio Orione <tulioorione@gmail.com>
2026-05-20 18:03:33 -0300

    Adiciona base URL dinâmica para recursos CSS

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
+<?php
+    include_once("helpers/url.php");
+?>
 <!DOCTYPE html>
 <html lang="pt-bt">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Codar</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="<?= $BASE_URL; ?>/css/style.css">
 </head>
 <body>
     
