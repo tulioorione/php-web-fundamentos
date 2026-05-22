72910f7117d128524c84e08efb73425125310fe3
Tulio Orione <tulioorione@gmail.com>
2026-05-22 10:36:02 -0300

    Adiciona estilos para página inicial do blog

--- templates/header.php
+++ templates/header.php
@@ -11,7 +11,7 @@ $BASE_URL = $BASE_URL ?? "";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Codar</title>
     <!-- ESTILIZAÇÃO DO PROJETO -->
-    <link rel="stylesheet" href="<?= $BASE_URL; ?>/css/style.css">
+    <link rel="stylesheet" href="<?= $BASE_URL; ?>css/style.css">
     <!-- GOOGLE FONSTS -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
