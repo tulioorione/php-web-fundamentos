d7c0a2871dca58ede9256dc466b1e525155412c5
Tulio Orione <tulioorione@gmail.com>
2026-05-22 10:56:13 -0300

    Adiciona página para exibição de post individual com tags e categorias

---
+++ post.php
@@ -0,0 +1,48 @@
+<?php
+  include_once("helpers/url.php");
+  include_once("data/posts.php");
+  include_once("templates/header.php");
+  include_once("data/categories.php");
+
+  $currentPost = null;
+
+  if(isset($_GET['id'])) {
+    $postId = (int) $_GET['id'];
+    
+    foreach($posts as $post) {
+        if($post['id'] == $postId) {
+            $currentPost = $post;
+            break;
+        }
+    }
+  }
+?>
+  
+  <main id="post-container">
+    <div class="content-container">
+        <h1 id="main-title"><?= $currentPost['title'] ?></h1>
+        <p id="post-description"><?= $currentPost['description'] ?></p>
+        <div class="img-container">
+            <img src="<?= $BASE_URL ?>/img/<?= $currentPost['img'] ?>" alt="<?= $currentPost['title'] ?>">
+        </div>
+        <p class="post-content"></p>
+    </div>
+  </main>
+  <aside id="nav-container">
+    <h3 id="tags-title">Tags</h3>
+    <ul id="tag-list">
+        <?php foreach($currentPost['tags'] as $tag): ?>
+            <li><a href="#"><?= $tag ?></a></li>
+        <?php endforeach; ?>
+    </ul>
+    <h3 id="categories-title">Categorias</h3>
+    <ul id="categories-list">
+        <?php foreach($categories as $category): ?>
+            <li><a href="#"><?= $category ?></a></li>
+        <?php endforeach; ?>        
+    </ul>
+  </aside>
+
+<?php
+  include_once("templates/footer.php");
+?>
